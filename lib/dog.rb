52ab68dd2254dadb059e978fadb2b225a1b70e26
Savannah C <sgcorrell@gmail.com>
2019-03-27 16:10:36 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name
+    @name
+  end
+  
+  def name=(given_name)
+    @name = given_name
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+end
